4889a8844941b4ef32353ae90d584dff8e88f0c3
Michael Sevilla <mikesevilla3@gmail.com>
2015-12-15 16:55:09 -0800

    Collapse code into one server
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- src/Clients/BaseballClient.java
+++ src/Clients/BaseballClient.java
@@ -5,26 +5,28 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
+import Clients.Client.MyRunnable;
 import Proxy.Client_Proxy_interface;
 
-public class Client implements Runnable{
-	static Client cl;
+public class BaseballClient implements Runnable{
+	static BaseballClient cl;
 	static final int STRONGCONSISTENCY = 0;
 	static final int EVENTUALCONSISTENCY = 1;
 	static final int MONOTONIC = 2;
 	static final int STALEBOUNDEDNESS = 3;
+	static int keys[] = {0,1};
 	public static void main(String args[])
 	{
-		cl = new Client();
+		cl = new BaseballClient();
 		Client_Proxy_interface cpinterface = null;
 		try	{
 			cpinterface = (Client_Proxy_interface)(Naming.lookup("client_proxy"));
-			int i = 10;
+			int i = 0;
 		//	long currentTimeinMillis = System.currentTimeMillis();
-			while(i>0)
+			while(i<2)
 			{
-				i--;
-				cpinterface.put(i, Thread.currentThread().getName() + " index "+i);	
+				cpinterface.put(keys[i], Thread.currentThread().getName() + " index "+i);	
+				i++;
 			}
 		//	System.out.println(Thread.currentThread().getName() + " time taken is " + (System.currentTimeMillis() - currentTimeinMillis));
 		} catch (IOException | NotBoundException e) {
@@ -38,6 +40,15 @@ public class Client implements Runnable{
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+	}
+	
+	void startWriteClients(){
+		Thread clientThread1 = new Thread(new BaseballClient());
+		clientThread1.setName("Thread1");
+		clientThread1.start();
+		/*Thread clientThread2 = new Thread(new BaseballClient());
+		clientThread2.setName("Thread2");
+		clientThread2.start();*/
 	}
 	
 	void startReadClients(){
@@ -64,48 +75,8 @@ public class Client implements Runnable{
 		}
 	}
 	
-	void startWriteClients(){
-		Thread clientThread1 = new Thread(new Client());
-		clientThread1.setName("Thread1");
-		clientThread1.start();
-		/*Thread clientThread2 = new Thread(new Client());
-		clientThread2.setName("Thread2");
-		clientThread2.start();*/
-	}
-	
-/*	void startReadClients(int readType){
-		Thread strongReadThread1 = new Thread(new Runnable() {
-			int type = readType;
-			int randomKeys[] = new int[1];
-			public void run() {
-				try	{
-					Client_Proxy_interface cpinterface = (Client_Proxy_interface)(Naming.lookup("client_proxy"));
-					int i = 300;
-					long currentTimeinMillis = System.currentTimeMillis();
-					while(i>0)
-					{
-						i--;
-						randomKeys[0] = (int)(Math.random()*10);
-						String str[] = cpinterface.get(randomKeys, type);
-						System.out.print("read" );
-						for(int j = 0; j < str.length; j++){
-							System.out.print(" " + str[j]);
-						}
-						System.out.println();
-					}
-					//System.out.println(Thread.currentThread().getName() + " time taken is " + (System.currentTimeMillis() - currentTimeinMillis));
-				} catch (IOException | NotBoundException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		strongReadThread1.setName("ReadThread1");
-		strongReadThread1.start();
-	}*/
-	
 	class MyRunnable implements Runnable{
 		int type = 0;
-		int randomKeys[] = new int[1];
 		public MyRunnable(int types) {
 			type = types;
 		}
@@ -119,8 +90,7 @@ public class Client implements Runnable{
 				while(i>0)
 				{
 					i--;
-					randomKeys[0] = (int)(Math.random()*10);
-					String str[] = cpinterface.get(randomKeys, type);
+					String str[] = cpinterface.get(keys, type);
 					System.out.print("read " );
 					for(int j = 0; j < str.length; j++){
 						System.out.print(" " + str[j]);
@@ -143,12 +113,12 @@ public class Client implements Runnable{
 	void makeConnection(){
 	try	{
 			Client_Proxy_interface cpinterface = (Client_Proxy_interface)(Naming.lookup("client_proxy"));
-			int i = 200;
+			int i = 0;
 			long currentTimeinMillis = System.currentTimeMillis();
-			while(i>0)
+			while(i<=200)
 			{
-				i--;
-				cpinterface.put((int)(Math.random()*10), " value "+i);	
+				cpinterface.put((int)(Math.random()*2), " value "+i);
+				i++;
 			}
 			System.out.println(Thread.currentThread().getName() + " time taken is " + (System.currentTimeMillis() - currentTimeinMillis));
 		} catch (IOException | NotBoundException e) {
--- src/Clusters/Proxy_Cluster_interface.java
+++ src/Clusters/Proxy_Cluster_interface.java
@@ -2,11 +2,12 @@ package Clusters;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.concurrent.locks.Lock;
 
 public interface Proxy_Cluster_interface extends Remote {
-	public int put(int key, String value) throws RemoteException;
-	public String get(int key) throws RemoteException;
-	public boolean takeKey() throws RemoteException;
-	public void releaseKey() throws RemoteException;
-	
+	public int put(int key[], String value[]) throws RemoteException;
+	public String[] get(int key[], int type, long arg[]) throws RemoteException;
+	public long takeLock(int key[]) throws RemoteException;
+	public void releaseLock(int key[]) throws RemoteException;
+	public String[] getTimeStamp(int key[], int type) throws RemoteException;
 }
